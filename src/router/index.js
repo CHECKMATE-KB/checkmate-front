9b37dbd9cf858ddaac21ffc595d73c2b6d55aa1f
Hongse0 <hongseyoung981007@gmail.com>
2024-11-20 15:51:59 +0900

    feat :  퀴즈페이지 백 연결

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '../pages/HomePage.vue';
-import QuizPage from '../pages/QuizPage.vue'; // 퀴즈 페이지 컴포넌트 임포트
-import UserJoin from '../pages/UserJoin.vue'; // 회원가입 페이지 컴포넌트 임포트
-import Login from '../pages/Login.vue'; // 로그인 페이지 컴포넌트 임포트
+import QuizPage from '../pages/QuizPage.vue';
+import UserJoin from '../pages/UserJoin.vue';
+import Login from '../pages/Login.vue';
 import FindMistake from '../pages/FindMistake.vue';
 import FindMistakeResult from '../pages/FindMistakeResult.vue';
-
 import Mypage from '../pages/Mypage.vue';
-
-import Challenge from '../pages/challenge/Challenge.vue'
-import ChallengeInfo from '../pages/challenge/ChallengeInfo.vue'
-import ChallengeDetail from '../pages/challenge/ChallengeDetail.vue'
-import ChallengeStart from '../pages/challenge/ChallengeStart.vue'
-import ChallengeCreate from '../pages/challenge/ChallengeCreate.vue'
+import Challenge from '../pages/challenge/Challenge.vue';
+import ChallengeInfo from '../pages/challenge/ChallengeInfo.vue';
+import ChallengeDetail from '../pages/challenge/ChallengeDetail.vue';
+import ChallengeStart from '../pages/challenge/ChallengeStart.vue';
+import ChallengeCreate from '../pages/challenge/ChallengeCreate.vue';
+import FinalQuizResult from '../pages/FinalResult.vue';
 
 const routes = [
-
   { path: '/', name: 'HomePage', component: HomePage },
   { path: '/quiz', name: 'quiz', component: QuizPage },
   { path: '/findMistake', name: 'findMistake', component: FindMistake },
   { path: '/findMistakeResult', name: 'findMistakeResult', component: FindMistakeResult },
-
   { path: '/challenge', name: 'Challenge', component: Challenge },
   { path: '/challenge/info', name: 'ChallengeInfo', component: ChallengeInfo },
   { path: '/challenge/detail', name: 'ChallengeDetail', component: ChallengeDetail },
@@ -30,46 +27,11 @@ const routes = [
   { path: '/userjoin', name: 'UserJoin', component: UserJoin },
   { path: '/login', name: 'Login', component: Login },
   { path: '/mypage', name: 'Mypage', component: Mypage },
-  
-
-
-  {
-    path: '/',
-    name: 'home',
-    component: HomePage,
-  },
-  {
-    path: '/quiz', // 퀴즈 페이지의 경로
-    name: 'quiz', // 퀴즈 페이지의 이름
-    component: QuizPage, // 퀴즈 페이지 컴포넌트
-  },
- 
-  {
-    path: '/login', // 로그인 페이지의 경로
-    name: 'Login', // 결과 페이지의 이름
-    component: Login, // 결과 페이지 컴포넌트
-  },
-  {
-    path: '/userjoin', // 회원가입 페이지의 경로
-    name: 'UserJoin', // 회원가입 페이지의 이름
-    component: UserJoin, // 회원가입 페이지 컴포넌트
-  },
-  {
-    path: '/findMistake',
-    name: 'findMistake',
-    component: FindMistake,
-  },
-  {
-    path: '/findMistakeResult',
-    name: 'findMistakeResult',
-    component: FindMistakeResult,
-  },
-
-
+  { path: '/finalQuizResult', name: 'finalQuizResult', component: FinalQuizResult },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'), // BASE_URL 명시적으로 설정
   routes,
 });
 

84caff13db4a1f9a0f8ddac5dfe14341b2404337
HwangHyeonseok <hhs0995@naver.com>
2024-11-21 13:42:35 +0900

    fix:footer modify

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -4,13 +4,13 @@
           <div class="footer-left w-100">
               <img class="logo" src="../../assets/images/cmlogo2.png" alt="Logo" />
               <div class="w-100">
-                  <h3 class="h3" style="color: white">KB IT's Your Life 5기 BUDUNGS</h3>
+                  <h3 class="h3" style="color: black">KB IT's Your Life 5기 BUDUNGS</h3>
                   <div class="fs-sm mb-1">서울 광진구 능동로 195-16 6층 (멀티캠퍼스 세종대)</div>
                   <div class="fs-sm mb-1">Designed by 이조은, 정단호, 정예슬, 홍세영, 황현석</div>
                   <div class="fs-sm mb-1 w-100 d-flex">
                       <div>Developed by 이조은, 정단호, 정예슬, 홍세영, 황현석</div>
                       <div style="margin-left:auto">
-                          <a href="https://github.com/CHECKMATE-KB" class="github-link" target="_blank"><i class="fab fa-github"></i> 프로젝트 보러가기</a>
+                          <a href="https://github.com/CHECKMATE-KB" class="github-link" target="_blank"><i class="fab fa-github" style="color: black"></i> 프로젝트 보러가기</a>
                       </div>
                   </div>
               </div>
@@ -38,7 +38,7 @@ export default {
 }
 */
 .footer {
-  background-color: #6e6e6e;
+  background-color: white;
   color: white;
   padding: 50px 0; /* 상하 여백만 설정 */
   font-family: 'Arial', sans-serif;
@@ -46,6 +46,8 @@ export default {
   position: relative; /* 명시적으로 relative 설정 */
   left: 0; /* 화면의 가장 왼쪽부터 시작 */
   margin: 0; /* margin 초기화 */
+  position: relative;
+  bottom: 100px;
 }
 
 .footer-container {
@@ -68,10 +70,13 @@ export default {
 }
 
 .github-link {
-  color: white;
+  color: black;
   text-decoration: none;
   display: flex;
   align-items: center;
+  position: relative;
+  right: 30px;
+  bottom: 35px;
 }
 
 .github-link i {
@@ -80,5 +85,8 @@ export default {
 
 .github-link:hover {
   text-decoration: underline;
+}
+.fs-sm {
+  color: black;
 }
 </style>
